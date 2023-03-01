83ac48d82d56e9bfbb865857727247edd7291409
luizhenriqque18 <luizhenriqque15@gmail.com>
2023-03-01 15:00:54 -0400

    build: config access for mongodb

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    databaseUrl: 'mongodb://localhost:27017/film-catalog',
     port: process.env.PORT || 5000,
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const config = require('./config');
 const apiRoutes = require('./routes/api');
 
@@ -7,6 +8,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.set("strictQuery", true);
+mongoose.connect(config.databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectado ao banco de dados.'))
+    .catch((e) => console.log('Erro ao conectar ao banco de dados.', e));
 
 app.use('/api', apiRoutes);
 
